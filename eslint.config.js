cfd5dd5434967e1ec609dd84a8c9804bfa3b156b
孔大夫 <kxb51812@gmail.com>
2025-04-08 20:03:37 +0800

    add css

--- eslint.config.js
+++ eslint.config.js
@@ -3,7 +3,7 @@ import antfu from '@antfu/eslint-config'
 export default antfu({
   formatters: true,
   astro: true,
-  ignores: ['src/assets/style.css'],
+  ignores: ['src/assets/style.css', 'src/assets/telegram.css'],
   rules: {
     'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
   },
